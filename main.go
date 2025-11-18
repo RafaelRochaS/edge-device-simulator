c17e9cdef48644ca39f17bac18a7bde638c2ab1c
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2025-11-18 16:05:50 -0300

    add: config parsing

--- main.go
+++ main.go
@@ -1,33 +1,84 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
 	"sort"
-	"strconv"
 	"time"
 
+	"github.com/google/uuid"
 	"gonum.org/v1/gonum/stat/distuv"
 )
 
+type Scenario int
+
+const (
+	Local Scenario = iota
+	Cloud
+	MEC
+)
+
+type Config struct {
+	Scenario    Scenario
+	Callback    string
+	ArrivalRate float64
+	Iterations  int
+}
+
+type CallbackData struct {
+	TaskID        string  `json:"taskId"`
+	DeviceID      string  `json:"deviceId"`
+	WorkloadSize  int     `json:"workloadSize"`
+	ExecutionSite string  `json:"executionSite"`
+	CreatedAt     int64   `json:"createdAt"`
+	Duration      float64 `json:"duration"`
+}
+
 func main() {
-	id := os.Getenv("DEVICE_ID")
+	id := uuid.New()
+	log.Println("Starting edge device simulator with id:", id)
 
-	parsedId, err := strconv.Atoi(id)
-	if err != nil {
-		parsedId = 0
+	config := getConfig()
+	log.Printf("Loaded config: %+v\n", config)
+
+	log.Println("Starting scenario...")
+
+	switch config.Scenario {
+	case Local:
+		scenarioOne(config)
+	default:
+		log.Fatalf("Scenario %d not yet implemented.", config.Scenario)
 	}
+}
 
-	log.Println("Starting edge device simulator with id:", parsedId)
+func getConfig() (config Config) {
+	scenarioFlag := flag.Int("scenario", 3, "Scenario to run:\n1 - Local processing\n2 - Cloud processing\n3 - Hybrid edge with xApp")
+	lambdaFlag := flag.Float64("arrival-rate", 0.5, "Arrival rate of workloads in requests per second.")
+	callbackFlag := flag.String("callback", "http://localhost", "Callback URL to send results to.")
+	iterationsFlag := flag.Int("duration", 100, "Total iterations of tasks to send.")
 
-	dist := distuv.Poisson{Lambda: 5}
+	flag.Parse()
 
-	maxAttempts := 100
-	workLoadSizes := make([]int, maxAttempts)
-	durations := make([]time.Duration, maxAttempts)
+	if *scenarioFlag < 1 || *scenarioFlag > 3 {
+		config.Scenario = MEC
+	}
+	config.Scenario = Scenario(*scenarioFlag)
 
-	for i := 0; i < maxAttempts; i++ {
-		n := int(dist.Rand()*50_000_000) + 1
+	config.ArrivalRate = *lambdaFlag
+	config.Callback = *callbackFlag
+	config.Iterations = *iterationsFlag
+
+	return
+}
+
+func scenarioOne(config Config) {
+	dist := distuv.Poisson{Lambda: config.ArrivalRate}
+
+	workLoadSizes := make([]int, config.Iterations)
+	durations := make([]time.Duration, config.Iterations)
+
+	for i := 0; i < config.Iterations; i++ {
+		n := int(dist.Rand()*50_000) + 1
 		workLoadSizes[i] = n
 		durations[i] = CpuBoundWork(n)
 	}
@@ -39,4 +90,13 @@ func main() {
 
 	log.Println("Work load sizes:", workLoadSizes)
 	log.Println("Work load durations:", durations)
+}
+
+func getCallbackData(workloadSize int) CallbackData {
+	return CallbackData{
+		TaskID:        uuid.New().String(),
+		WorkloadSize:  workloadSize,
+		ExecutionSite: "edge",
+		CreatedAt:     time.Now().Unix(),
+	}
 }
